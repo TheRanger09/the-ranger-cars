1f0f144ede1d46ae1d0e10f992ae3ffa20773a47
TheRanger09 <sy.nadim07@gmail.com>
2026-05-12 15:56:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function showCar(car){
 
+  const data = {
+    bugatti:"Bugatti Chiron: 1500HP hypercar with 420 km/h top speed.",
+    gtr:"Nissan GT-R: Twin turbo legend known as Godzilla.",
+    lambo:"Lamborghini Aventador: V12 beast with 770HP.",
+    ferrari:"Ferrari SF90: Hybrid hypercar with futuristic tech."
+  };
+
+  document.getElementById("carInfo").innerHTML =
+  `<h2>Car Info</h2><p>${data[car]}</p>`;
+}
+
+function toggleTheme(){
+  document.body.classList.toggle("light-mode");
+}
